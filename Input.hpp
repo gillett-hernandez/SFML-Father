83c1ee500102da6d384fac0315f3f615c7ab628c
Estevan Hernandez <estevan@bluerocket.us>
2017-03-02 11:43:01 -0800

    moving some code to the Input.hpp file, instead of having it live in main.cpp

--- Input.hpp
+++ Input.hpp
@@ -15,4 +15,53 @@ struct INPUT_STRUCT {
         bool rightReleased = false;
     };
 typedef INPUT_STRUCT InputStruct;
+void clearInput(InputStruct &input) {
+    input.upPressed = false;
+        input.downPressed = false;
+        input.leftPressed = false;
+        input.rightPressed = false;
+
+        input.upReleased = false;
+        input.downReleased = false;
+        input.leftReleased = false;
+        input.rightReleased = false;
+}
+void logForInput(InputStruct &input) {
+    if (input.upPressed) {
+        std::cout << "upPressed" << std::endl;
+    }
+    if (input.leftPressed) {
+        std::cout << "leftPressed" << std::endl;
+    }
+    if (input.downPressed) {
+        std::cout << "downPressed" << std::endl;
+    }
+    if (input.rightPressed) {
+        std::cout << "downPressed" << std::endl;
+    }
+    if (input.up) {
+        std::cout << "up" << std::endl;
+    }
+    if (input.left) {
+        std::cout << "left" << std::endl;
+    }
+    if (input.down) {
+        std::cout << "down" << std::endl;
+    }
+    if (input.right) {
+        std::cout << "right" << std::endl;
+    }
+    if (input.upReleased) {
+        std::cout << "upReleased" << std::endl;
+    }
+    if (input.leftReleased) {
+        std::cout << "leftReleased" << std::endl;
+    }
+    if (input.rightReleased) {
+        std::cout << "rightReleased" << std::endl;
+    }
+    if (input.downReleased) {
+        std::cout << "downReleased" << std::endl;
+    }
+}
 #endif
--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 const bool debug = true;
 
 using namespace std;
-void logForInput(InputStruct &input);
+
 int main(int argc, char const *argv[]) {
     std::cout << "Arguments: " << argc;
     for (int i = 0; i < argc; i++) {
@@ -19,15 +19,7 @@ int main(int argc, char const *argv[]) {
     InputStruct input;
 
     while (window.isOpen()) {
-        input.upPressed = false;
-        input.downPressed = false;
-        input.leftPressed = false;
-        input.rightPressed = false;
-
-        input.upReleased = false;
-        input.downReleased = false;
-        input.leftReleased = false;
-        input.rightReleased = false;
+        clearInput(input);
 
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -106,43 +98,4 @@ int main(int argc, char const *argv[]) {
     }
 
     return EXIT_SUCCESS;
-}
-
-void logForInput(InputStruct &input) {
-    if (input.upPressed) {
-        std::cout << "upPressed" << std::endl;
-    }
-    if (input.leftPressed) {
-        std::cout << "leftPressed" << std::endl;
-    }
-    if (input.downPressed) {
-        std::cout << "downPressed" << std::endl;
-    }
-    if (input.rightPressed) {
-        std::cout << "downPressed" << std::endl;
-    }
-    if (input.up) {
-        std::cout << "up" << std::endl;
-    }
-    if (input.left) {
-        std::cout << "left" << std::endl;
-    }
-    if (input.down) {
-        std::cout << "down" << std::endl;
-    }
-    if (input.right) {
-        std::cout << "right" << std::endl;
-    }
-    if (input.upReleased) {
-        std::cout << "upReleased" << std::endl;
-    }
-    if (input.leftReleased) {
-        std::cout << "leftReleased" << std::endl;
-    }
-    if (input.rightReleased) {
-        std::cout << "rightReleased" << std::endl;
-    }
-    if (input.downReleased) {
-        std::cout << "downReleased" << std::endl;
-    }
 }
